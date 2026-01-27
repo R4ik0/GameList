928c50bbb6ceb3b9e198b28eedf24d806a94c051
Bastien Vigneron <raiko352003@gmail.com>
2026-01-27 16:05:56 +0100

    Change CORS settings to allow all origins
    Updated CORS middleware to allow all origins.

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ app = FastAPI(title="GameList API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",      # live-server
-        "http://127.0.0.1:5500",
-        "https://r4ik0.github.io",    # GitHub Pages
-        "https://r4ik0.github.io/GameList",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
