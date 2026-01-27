364c4344edf1d614a6950373bccf4e2a77061ece
MVchara <vannimarco2003@gmail.com>
2026-01-27 12:04:50 +0100

    /full_game combine /get_game and /get_attribute_name, fixed /get_attribute_game so that it only sends the name

--- src/routers/igdb_router.py
+++ src/routers/igdb_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from src.models.game import get_game_from_igdb, get_X_games, search_game, get_name_from_attribute_id
+from src.models.game import Game,get_game_from_igdb, get_X_games, search_game, get_name_from_attribute_id
 
 router = APIRouter(prefix="", tags=["igdb"])
 
@@ -24,3 +24,17 @@ async def get_games(X: int):
 async def get_games_from_name(name: str):
     game_list = search_game(name)
     return game_list
+
+@router.post("/full_game")
+async def get_full_game(id: int):
+    game = get_game_from_igdb(id)
+    genres,themes,game_modes,platforms = [], [], [], []
+    for i in game.genres:
+        genres.append(get_name_from_attribute_id('genres', i))
+    for i in game.themes:
+        themes.append(get_name_from_attribute_id('themes', i))
+    for i in game.game_modes:
+        game_modes.append(get_name_from_attribute_id('game_modes', i))
+    for i in game.platforms:
+        platforms.append(get_name_from_attribute_id('platforms', i))
+    return Game(id =game.id,name = game.name,genres = genres,themes = themes,game_modes = game_modes,platforms = platforms,storyline = game.storyline)
